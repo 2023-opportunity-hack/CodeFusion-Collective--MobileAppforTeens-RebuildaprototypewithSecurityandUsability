dba294fa68db07aefcd4239b9614560f1fb804ce
David Tran <davidtran3237@gmail.com>
2023-10-07 13:11:19 -0700

    create navigation folders

---
+++ app/test.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { Text , View } from '../components/Themed';
+
+export default function Test() {
+    return (
+        <View>
+            <Text>Test Page</Text>
+        </View>
+    )
+}
