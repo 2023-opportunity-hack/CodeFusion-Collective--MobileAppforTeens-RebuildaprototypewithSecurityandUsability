e0e9cfe3aa3da9ddcbc53418c1e7cfb6b8f0602f
David Tran <davidtran3237@gmail.com>
2023-10-07 17:09:47 -0700

    temp commit

---
+++ app/lockscreen/_layout.tsx
@@ -0,0 +1,10 @@
+// import { Link, Slot } from "expo-router";
+// import { useColorScheme } from "react-native";
+
+// export default function LockscreenLayout() {
+//     const colorScheme = useColorScheme
+
+//     return (
+//         <Slot />
+//     )
+// }
