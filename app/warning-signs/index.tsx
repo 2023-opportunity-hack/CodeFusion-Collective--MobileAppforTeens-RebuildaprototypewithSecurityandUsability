bed0a1e0de21cfe7ea9bc5c176cb729c3d13c4eb
John Novakowski <jnovakowski797@gmail.com>
2023-10-08 03:36:47 -0400

    Added scrolling through the page to Warning Signs

--- app/warning-signs/index.tsx
+++ app/warning-signs/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
 import { Link } from 'expo-router';
 import DomesticViolenceText from '../../components/warning-signs/DomesticViolenceText';
 import EconomicAbuseText from '../../components/warning-signs/EconomicAbuseText';
@@ -24,80 +24,82 @@ export default function AnotherComponent () {
   const [showMoreTechnologicalAssistedAbuse, setShowMoreTechnologicalAssistedAbuse] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.backButton}>
-        <Link href='/homepage' style={{marginRight: '5%', marginTop: '8%'}}>
-          <Image source={require('../../assets/images/Back.png')}   style={{width: 20, height: 20, marginTop: '5%'}} />
-        </Link>
-        <Text style={styles.title}>Warning Signs</Text>
-      </View>
-      <Text style={styles.description}>It is not always obvious when someone you care about has been affected by sexual violence. Learning the warning signs for children, teens, and college-age adults can help you learn when to act.</Text>
-      <TouchableOpacity onPress={() => setShowMoreDomesticViolence(!showMoreDomesticViolence)}>
-        <View style={styles.dropDownButtonOne}>
-          <Text>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.backButton}>
+          <Link href='/homepage' style={{marginRight: '5%', marginTop: '8%'}}>
+            <Image source={require('../../assets/images/Back.png')}   style={{width: 20, height: 20, marginTop: '5%'}} />
+          </Link>
+          <Text style={styles.title}>Warning Signs</Text>
+        </View>
+        <Text style={styles.description}>It is not always obvious when someone you care about has been affected by sexual violence. Learning the warning signs for children, teens, and college-age adults can help you learn when to act.</Text>
+        <TouchableOpacity onPress={() => setShowMoreDomesticViolence(!showMoreDomesticViolence)}>
+          <View style={styles.dropDownButtonOne}>
+            <Text>
             {showMoreDomesticViolence ? <DomesticViolenceText/> : 'What is domestic violence?'}
-          </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '40%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-      <Text>Types of abuse</Text>
-      <TouchableOpacity onPress={() => setShowMoreEmotionalAbuse(!showMoreEmotionalAbuse)}>
-      <View style={styles.dropDownButtonTop}>
-        <Text>
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '40%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <Text>Types of abuse</Text>
+        <TouchableOpacity onPress={() => setShowMoreEmotionalAbuse(!showMoreEmotionalAbuse)}>
+        <View style={styles.dropDownButtonTop}>
+          <Text>
           {showMoreEmotionalAbuse ? <EmotionalAbuseText/> : 'Emotional abuse'}
-        </Text>
-        <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '61%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMoreGaslighting(!showMoreGaslighting)}>
-      <View style={styles.dropDownButtonMiddle}>
-        <Text>
+          </Text>
+          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '61%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMoreGaslighting(!showMoreGaslighting)}>
+        <View style={styles.dropDownButtonMiddle}>
+          <Text>
           {showMoreGaslighting ? <GaslightingText/> : 'Gaslighting'}
-        </Text>
-        <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '72%'}}resizeMode='contain'/>
-      </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMoreReproductiveCoersion(!showMoreReproductiveCoersion)}>
-        <View style={styles.dropDownButtonMiddle}>
-          <Text>
-          {showMoreReproductiveCoersion ? <ReproductiveCoercionText/> : 'Reproductive coercion'}
           </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '49%'}}resizeMode='contain'/>
+          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '72%'}}resizeMode='contain'/>
         </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMorePhysicalAbuse(!showMorePhysicalAbuse)}>
-        <View style={styles.dropDownButtonMiddle}>
-          <Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMoreReproductiveCoersion(!showMoreReproductiveCoersion)}>
+          <View style={styles.dropDownButtonMiddle}>
+            <Text>
+            {showMoreReproductiveCoersion ? <ReproductiveCoercionText/> : 'Reproductive coercion'}
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '49%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMorePhysicalAbuse(!showMorePhysicalAbuse)}>
+          <View style={styles.dropDownButtonMiddle}>
+            <Text>
           {showMorePhysicalAbuse ? <PhysicalAbuseText/> : 'Physical abuse'}
-          </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '64%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMoreSexualAbuse(!showMoreSexualAbuse)}>
-        <View style={styles.dropDownButtonMiddle}>
-          <Text>
-          {showMoreSexualAbuse ? <SexualAbuseText/> : 'Sexual abuse'}
-          </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '67%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMoreEconomicAbuse(!showMoreEconomicAbuse)}>
-        <View style={styles.dropDownButtonMiddle}>
-          <Text>
-          {showMoreEconomicAbuse ? <EconomicAbuseText/> : 'Economic abuse'}
-          </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '60.5%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setShowMoreTechnologicalAssistedAbuse(!showMoreTechnologicalAssistedAbuse)}>
-        <View style={styles.dropDownButtonBottom}>
-          <Text>
-          {showMoreTechnologicalAssistedAbuse ? <TechnologicalAssistedAbuseText/> : 'Technological-assisted abuse'}
-          </Text>
-          <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '34%'}}resizeMode='contain'/>
-        </View>
-      </TouchableOpacity>
-    </View>
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '64%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMoreSexualAbuse(!showMoreSexualAbuse)}>
+          <View style={styles.dropDownButtonMiddle}>
+            <Text>
+            {showMoreSexualAbuse ? <SexualAbuseText/> : 'Sexual abuse'}
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '67%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMoreEconomicAbuse  (!showMoreEconomicAbuse)}>
+          <View style={styles.dropDownButtonMiddle}>
+            <Text>
+            {showMoreEconomicAbuse ? <EconomicAbuseText/> : 'Economic abuse'}
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '60.5%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowMoreTechnologicalAssistedAbuse(!showMoreTechnologicalAssistedAbuse)}>
+          <View style={styles.dropDownButtonBottom}>
+            <Text>
+            {showMoreTechnologicalAssistedAbuse ?     <TechnologicalAssistedAbuseText/> : 'Technological-assisted abuse'}
+            </Text>
+            <Image source={require('../../assets/images/Back.png')}     style={{width: 20, height: 20, transform: [{ rotate: '270deg' }], marginLeft: '34%'}}resizeMode='contain'/>
+          </View>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
