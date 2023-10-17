908c3e8b4fd3620a97b53091d55c8e88e1201700
John Novakowski <jnovakowski797@gmail.com>
2023-10-17 19:18:35 -0400

    Domestic Violence Placeholder Text replaced with UX provided text

--- components/warning-signs/DomesticViolenceText.tsx
+++ components/warning-signs/DomesticViolenceText.tsx
@@ -7,7 +7,9 @@ export default function DomesticViolenceText() {
       <Text style={warningSignStyles.title}>What is domestic violence?</Text>
       <ScrollView style={warningSignStyles.descriptionContainer}>
         <Text style={warningSignStyles.description}>
-        <Text style={{ fontWeight: 'bold' }}>Domestic violence </Text>is PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER PLACEHOLDER
+        <Text style={{ fontWeight: 'bold' }}>Domestic violence </Text>
+        is a pattern of assaultive and coercive behaviors through physical, sexual, emotional, psychological, and or economic abuse by one person in a current or former intimate relationship to maintain power and control over the other.{'\n'}
+        Domestic violence knows no boundaries. People of all ages, genders, cultures, religions, professions, abilities, and income levels experience domestic violence. However, systemic racism, poverty, immigration status, disability, and other inequities can make the risks even more severe, especially for BIPOC and LGBTQ individuals.
         </Text>
       </ScrollView>
     </ScrollView>
