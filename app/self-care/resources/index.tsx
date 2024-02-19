cf9b8f0d5cb29fd0fc7b51384a1bb0ab66d8b575
Gabe Jimenez <jimenezgabriel12@gmail.com>
2024-02-19 02:05:56 -0700

    Add loading spinner to video boxes

--- app/self-care/resources/index.tsx
+++ app/self-care/resources/index.tsx
@@ -1,8 +1,15 @@
-import { Link } from "expo-router"
-import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native"
-import YoutubePlayer from "react-native-youtube-iframe"
+import { Link } from "expo-router";
+import { useState } from "react";
+import { Image, Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 const Resources = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleVideoReady = () => {
+    setLoading(false);
+  }
 
   return (
     <ScrollView style={styles.container}>
@@ -19,14 +26,17 @@ const Resources = () => {
       </View>
       <Text style={{ fontFamily: "JakartaSemiBold" }}>These videos may be helpful for additional support and strategies in maintaining your mental health</Text>
       <View style={styles.video}>
-        <YoutubePlayer
-          height={195}
-          videoId="wr4N-SdekqY"
-          webViewStyle={{ borderRadius: 9, }}
-        />
-      </View>
+        {loading && <ActivityIndicator animating={loading} size="small" style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }} />}
+         <YoutubePlayer
+            height={195}
+            videoId="wr4N-SdekqY"
+            webViewStyle={{ borderRadius: 9, }}
+            onChangeState={handleVideoReady}
+          />
+        </View>
       <Text style={{ marginBottom: 30, fontFamily: "JakartaSemiBold" }}>Mental Health Minute video</Text>
       <View style={styles.video}>
+        {loading && <ActivityIndicator animating={loading} size="small" style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }} />}
         <YoutubePlayer
           height={195}
           videoId="7Ep5mKuRmAA"
